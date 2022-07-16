6e2355e59f40bd7ede61e3ce3d0518574ef4cff0
nozomi-nakamitsu <nozomi_nakamitsu@arsaga.jp>
2022-07-16 14:03:45 +0900

    feat: first-commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ test }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, SetupContext } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  props: {},
+  setup(props, context: SetupContext) {
+    /**
+     * test
+     */
+    const test = "test";
+
+    return {
+      test,
+    };
+  },
+});
+</script>
